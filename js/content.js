5e1db9b809a71d61ea6c85eecd268f5493c3e377
Anirudh Kulkarni <k.anirudh8@gmail.com>
2019-03-21 14:20:56 -0400

    Logic to check signup | jquery powered

--- js/content.js
+++ js/content.js
@@ -6,6 +6,11 @@ chrome.runtime.onMessage.addListener(
             // this is required to use sendResponse asynchronously
             return true;
         }
+        if (request.action === "checkForSignup") {
+            checkForSignup(request, sender, sendMessage);
+            // this is required to use sendResponse asynchronously
+            return true;
+        }
     }
 );
 
@@ -37,3 +42,101 @@ function checkForPassword(request, sender, sendMessage) {
     console.log(ele.toString());
     return sendMessage(false);
 }
+
+/*
+ * Rules to identify a signup page : 
+ * 1. The window.location has signup in its path
+ * 2. There exists a form element in the html dom with
+ *      a. method == post
+ *           i. action, id, or name containing words like ['signup']
+ *          ii. form containing button type = 'submit' with value or name in ['signup','create','create account','register']
+ *         iii. has atleast 1 input type = 'email' 
+ *                  atleast 1 input type = 'password' 
+ *                  atleast 1 buttom type = 'submit'
+ *              and atleast 1 other non hidden input type with name in 
+ *                  ['first name', 'mobile number', 'phone number', 'last name', 'name', 'gender', 'dob', 'birth', 'date']  
+ * 
+ * Works for :
+ *      - Facebook      : https://www.facebook.com/
+ *      - Yahoo         : https://login.yahoo.com/account/create?src=fpctx&intl=us&lang=en-US&done=https%3A%2F%2Fwww.yahoo.com&specId=yidReg
+ *      - Google        : https://accounts.google.com/signup/v2/webcreateaccount?hl=en-GB&flowName=GlifWebSignIn&flowEntry=SignUp&nogm=true
+ *      - Github        : https://github.com/
+ *      - Linkedin      : https://www.linkedin.com/
+ *      - Reddit        : https://www.reddit.com/
+ *      - Twilio        : https://www.twilio.com/try-twilio
+ *      - Amazon        : 
+ *      - 
+ * 
+ * Does not work for :
+ *      - Instacart
+ *      - Apple
+ */
+
+var extraStrings = ['name','gender','sex','number','age','birthday'],
+    signupStrings = ['signup','create account','register','sign up']
+    buttonStrings = ['signup','create account','register','sign up','join'];
+
+var regexExt = new RegExp(extraStrings.join( "|" ), "i"),
+    regex = new RegExp(signupStrings.join( "|" ), "i"),
+    regexBut = new RegExp(buttonStrings.join( "|" ), "i");
+
+var checkForSignup = function() {    
+    var formsList = document.getElementsByTagName('form');
+    for(let i = 0, len = formsList.length; i < len; i++) {
+        var form = formsList[i];
+        var method = form.method,
+            id = form.id,
+            action = form.action,
+            name = form.name,
+            className = form.className,
+            elements = form.elements;
+        var containsEmail = false,
+            containsPass = false,
+            containsSelect = false,
+            containsExtra = false;
+        var signup_form;
+
+        if(method == 'post' || method == 'get') {
+            if(regex.test(id) || regex.test(action) || regex.test(name) || regex.test(className)) {
+                console.log("Page contains signup in id, action, name or classname");
+                signup_form = form;
+                break;
+            } else {
+                for (let j = 0, element; element = elements[j++];) {
+                    let type = element.type;
+                    let fieldName = element.name;
+                    let className = element.className;
+
+                    if (type === "submit" && (regexBut.test(element.innerHTML) || regexBut.test(element.value))) {
+                        console.log("Page contains signup. Button label : " + element.innerHTML);
+                        signup_form = form;
+                        break;        
+                    }
+                    if (type === "email" || fieldName.includes("email") || fieldName.includes("id") || className.includes("email")) {
+                        console.log("Contains Email/userid");
+                        containsEmail = true;
+                    } 
+                    if (type === "password") {
+                        console.log("Contains Password");
+                        containsPass = true;
+                    } 
+                    if (regexExt.test(fieldName)) {
+                        console.log("Contains " + fieldName);
+                        containsExtra = true;
+                    }
+                    if (type === "select") {
+                        console.log("Contains Select");
+                        containsSelect = true;
+                    }
+                }
+                if(containsEmail && containsPass && (containsExtra || containsSelect)) {
+                    console.log("Page contains signup");
+                    signup_form = form;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+checkForSignup();
