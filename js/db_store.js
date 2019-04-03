963084388093836fe180e1ca93bf964b74e4f749
sjindal94 <shubhamjindal_1994@yahoo.com>
2019-04-03 00:05:57 -0400

    More fixes for initializations
    Signed-off-by: sjindal94 <shubhamjindal_1994@yahoo.com>

--- js/db_store.js
+++ js/db_store.js
@@ -21,11 +21,11 @@ function initDBForTest() {
     };
     dbEntries = [dbEntry1, dbEntry2, dbEntry3];
     writeBulkDocs(dbEntries);
-    readAllDocs();
 }
 
 function writeBulkDocs(docs) {
-    pouchDb.bulkDocs(docs, function (err, response) {
+    if(!pouchDb) console.log("DB Does not exist");
+    else pouchDb.bulkDocs(docs, function (err, response) {
         if (err) {
             return console.log(err);
         } else {
@@ -33,11 +33,13 @@ function writeBulkDocs(docs) {
             console.log("Documents created Successfully");
         }
     });
+    readAllDocs();
 }
 
 function readDoc(_id) {
     console.log("Reading just 1 entry with id:"+ _id);
-    pouchDb.get(_id, function (err, doc) {
+    if(!pouchDb) console.log("DB Does not exist");
+    else pouchDb.get(_id, function (err, doc) {
         if (err) {
             return console.log(err);
         } else {
@@ -46,8 +48,16 @@ function readDoc(_id) {
     });
 }
 
+function getDbInfo(){
+    if(!pouchDb) console.log("DB Does not exist");
+    else pouchDb.info().then(function (info) {
+        console.log(info);
+    })
+}
+
 function writeDoc(doc) {
-    pouchDb.put(doc, function (err, response) {
+    if(!pouchDb) console.log("DB Does not exist");
+    else pouchDb.put(doc, function (err, response) {
         if (err) {
             return console.log(err);
         } else {
@@ -60,7 +70,8 @@ function writeDoc(doc) {
 
 function removeDoc(_id, _rev) {
     //Deleting an existing document
-    pouchDb.remove(_id, _rev, function (err) {
+    if(!pouchDb) console.log("DB Does not exist");
+    else pouchDb.remove(_id, _rev, function (err) {
         if (err) {
             return console.log(err);
         } else {
@@ -71,7 +82,8 @@ function removeDoc(_id, _rev) {
 
 function readAllDocs() {
     //Retrieving all the documents in PouchDB
-    pouchDb.allDocs({include_docs: true, descending: true}, function (err, docs) {
+    if(!pouchDb) console.log("DB Does not exist");
+    else pouchDb.allDocs({include_docs: true, descending: true}, function (err, docs) {
         if (err) {
             return console.log(err);
         } else {
@@ -89,7 +101,8 @@ function createDB(){
 
 function destroyDB(){
     //deleting database
-    pouchDb.destroy(function (err, response) {
+    if(!pouchDb) console.log("DB Does not exist");
+    else pouchDb.destroy(function (err, response) {
         if (err) {
             return console.log(err);
         } else {
