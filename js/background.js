e9e7645b1b43c214ad62bffd833ee6e664413b76
sjindal94 <shubhamjindal_1994@yahoo.com>
2019-05-05 03:27:54 -0400

    Removing extra code
    Signed-off-by: sjindal94 <shubhamjindal_1994@yahoo.com>

--- js/background.js
+++ js/background.js
@@ -85,10 +85,6 @@ let tabListener = function () {
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (IS_ON && changeInfo.status === 'complete' && tab.active) {
-        let allLinks = document.links;
-        for (let i = 0; i < allLinks.length; i++) {
-            console.log("Link " + i + ": " + allLinks[i].href);
-        }
         console.log("Sending message to Content: processWebPage");
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {action: "processWebPage"}, function (response) {
